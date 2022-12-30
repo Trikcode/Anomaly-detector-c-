02ece9eb59a00aacab47a242f7b5e0bb3548e5b1
Trikcode <ayesiganobert@gmail.com>
2022-12-30 23:20:02 +0300

    anomaly detector

--- Program.cs
+++ Program.cs
@@ -1,18 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing.Text;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Net.NetworkInformation;
-using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
+using Azure;
+using Azure.AI.AnomalyDetector;
 using Azure.AI.AnomalyDetector.Models;
-using Azure.Core.TestFramework;
-using Microsoft.Identity.Client;
-using NUnit.Framework;
+using static System.Environment;
 
+namespace anomaly_detector_quickstart
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string endpoint = GetEnvironmentVariable("ANOMALY_DETECTOR_ENDPOINT");
+			string apiKey = GetEnvironmentVariable("ANOMALY_DETECTOR_API_KEY");
 
+			var endpointUri = new Uri(endpoint);
+			var credential = new AzureKeyCredential(apiKey);
 
+			//create client
+			AnomalyDetectorClient client = new AnomalyDetectorClient(endpointUri, credential);
 
+			//read data
+			//example: string datapath = @"c:\test\request-data.csv";
+			string datapath = @"request-data.csv";
+
+			List<TimeSeriesPoint> list = File.ReadAllLines(datapath, Encoding.UTF8)
+							.Where(e => e.Trim().Length != 0)
+							.Select(e => e.Split(','))
+							.Where(e => e.Length == 2)
+							.Select(e => new TimeSeriesPoint(float.Parse(e[1])) { Timestamp = DateTime.Parse(e[0]) }).ToList();
+
+			//create request
+			DetectRequest request = new DetectRequest(list)
+			{
+				Granularity = TimeGranularity.Daily
+			};
+
+			EntireDetectResponse result = client.DetectEntireSeries(request);
+
+			bool hasAnomaly = false;
+			for (int i = 0; i < request.Series.Count; ++i)
+			{
+				if (result.IsAnomaly[i])
+				{
+					Console.WriteLine("Anomaly detected at index: {0}.", i);
+					hasAnomaly = true;
+				}
+			}
+			if (!hasAnomaly)
+			{
+				Console.WriteLine("No anomalies detected in the series.");
+			}
+		}
+	}
+}
