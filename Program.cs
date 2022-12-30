ee83e1e6e8ec775faf8e37590d9ea5eef0702b2b
Trikcode <ayesiganobert@gmail.com>
2022-12-30 20:05:17 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.AI.AnomalyDetector.Models;
+using Azure.Core.TestFramework;
+using Microsoft.Identity.Client;
+using NUnit.Framework;
+
+
+
+
